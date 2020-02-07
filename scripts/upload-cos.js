d2c85405975cf1ed778a28c39111001c3ac43d94
刘荣 <liurong@liurongdeMacBook-Pro.local>
2020-02-07 14:32:38 +0800

    -

--- scripts/upload-cos.js
+++ scripts/upload-cos.js
@@ -10,57 +10,55 @@ const store = new COS({
 
 const baseDir = path.resolve(__dirname, '../build')
 
+let uploads = []
+
+function checkUploaded() {
+  return new Promise((resolve, reject) => {
+    store.getBucket({Bucket: '2019-ncov-1300334035', Region: 'ap-chengdu'}, (err, data) => {
+      if (err) return reject(err)
+      uploads = data.Contents.map(item => item.Key)
+      console.log(uploads)
+      return resolve(uploads)
+    });
+  })
+}
+
 async function uploadDir (dir) {
   const files = fs.readdirSync(path.resolve(baseDir, dir)).filter(x => !x.endsWith('map'))
   for (const file of files) {
     const fileName = path.join(dir, file)
+    if (uploads.indexOf(fileName) >= 0) continue
     const realFileName = path.resolve(baseDir, dir, file)
     const realFileStats = fs.statSync(realFileName)
     if (!realFileStats.isFile()) return
 
-    // 删除已有的
-    await new Promise((resolve, reject) => {
-      store.deleteObject({Key: fileName, Bucket: '2019-ncov-1300334035', Region: 'ap-chengdu'}, (err, data) => {
-        if (err) {
-          console.log(fileName + " : " + err)
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    console.log(fileName)
 
     await new Promise((resolve, reject) => {
       store.putObject({Key: fileName, Bucket: '2019-ncov-1300334035', Region: 'ap-chengdu', Body: fs.createReadStream(realFileName)}, (err, data) => {
-        if (err) {
-          console.log(fileName + " : " + err)
-          return reject(err)
-        }
+        if (err) return reject(err)
         return resolve(data)
       })
     })
-
-    // 如果是文件，则上传到 oss，目录跳过
-    // const isSkip = skip && await probeObjectMeta(path.join(dir, file))
-    // if (stats.isFile()) {
-    //   if (isSkip) {
-    //     console.info(`skip: ${path.join(dir, file)}`)
-    //   } else {
-    //     const o = await store.put(path.join(dir, file), path.resolve(baseDir, dir, file), {
-    //       headers
-    //     })
-    //     console.info(`done: ${o.name}`)
-    //   }
-    // }
   }
   return true
 }
 
-Promise.all([
-  uploadDir('.'),
-  uploadDir('static/media'),
-  uploadDir('static/js'),
-  uploadDir('static/css')
-]).catch(e => {
-  console.error(e)
-  process.exit(1)
-})
+async function run() {
+  try {
+    await checkUploaded()
+    console.log("开始上传：")
+    await Promise.all([
+      uploadDir('.'),
+      uploadDir('static/media'),
+      uploadDir('static/js'),
+      uploadDir('static/css')
+    ])
+    console.log('部署完成！')
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+}
+
+run()
